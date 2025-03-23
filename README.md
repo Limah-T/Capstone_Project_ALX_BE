ecdf4bc307e79f5556fb4e990119f04df97e20ca
Limah-T <limahtechnology@yahoo.com>
2025-03-23 19:27:29 +0100

    Set up forms and ensure form validations.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Capstone_Project_ALX_BE
-This repository is for my capstone project at ALX BE Cohort 4
--- database/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from .models import Staff
-
-# Create your views here.
-
-def home(request):
-    staffs = Staff.objects.all()
-    return render(request, 'individual_member.html', {'staffs': staffs})
